52b8ace69b59786b15a37c88622b4e1cd0c928c4
Jacob Tan <zhangen69@gmail.com>
2021-12-17 15:08:22 +0800

    sepreate with several functions

--- src/views/TronTest.vue
+++ src/views/TronTest.vue
@@ -1,47 +1,126 @@
 <script setup lang="ts">
-import { Ref, ref } from "vue-demi"
+import { Ref, ref } from "vue-demi";
 
-const tronWeb = (window as any).tronWeb
-const address = tronWeb.defaultAddress.base58
-const transferAddress = 'TNDrBYCjyxohQRfteRBdMzECCAhRcjewNV' // testing
-const apiKey = 'd099c4d3-bdd4-45b6-a246-bb2745492653'
-const sc = 'TKWsxjh97sCPipTAfghW7vDFdx2HhjecCe'
-const info: Ref<any> = ref({})
+const tronWeb = (window as any).tronWeb;
+const defaultAddress = tronWeb.defaultAddress.base58;
+const transferAddress = "TNDrBYCjyxohQRfteRBdMzECCAhRcjewNV"; // testing
+const apiKey = "d099c4d3-bdd4-45b6-a246-bb2745492653";
+const sc = "TKWsxjh97sCPipTAfghW7vDFdx2HhjecCe";
+const info: Ref<any> = ref({});
 
 // set header
-tronWeb.setHeader({ 'TRON-PRO-API-KEY': apiKey })
+tronWeb.setHeader({ "TRON-PRO-API-KEY": apiKey });
 
 // get contract
-let contract = ref()
+let contract = ref();
 const getContract = async () => {
-  contract.value = await tronWeb.contract().at(sc)
-  contract.value.tronWeb.setHeader({ 'TRON-PRO-API-KEY': apiKey })
-  console.log('contract', contract.value)
-}
+  contract.value = await tronWeb.contract().at(sc);
+  contract.value.tronWeb.setHeader({ "TRON-PRO-API-KEY": apiKey });
+  console.log("contract", contract.value);
+};
 
 const getContractMethod = async (method: any) => {
   if (!contract.value) {
-    await getContract()
+    await getContract();
   }
   if (!method) {
-    alert('method cannot be empty')
+    alert("method cannot be empty");
   }
-  const func = contract.value[method]
+  const func = contract.value[method];
   if (func) {
-    if (method == 'balanceOf') {
-      const result = await func(address).call()
-      info.value[method] = result.toString() / 100
-    } else if(method == 'transfer') {
+    if (method == "balanceOf") {
+      const result = await func(address).call();
+      info.value[method] = result.toString() / 100;
+    } else if (method == "transfer") {
       try {
-        const result = await func(transferAddress, 10000).send({ feeLimit: 5e9 })
-        info.value[method] = result
+        const result = await func(transferAddress, 10000).send({
+          feeLimit: 5e9,
+        });
+        info.value[method] = result;
       } catch (error) {
-        info.value[method] = error
+        info.value[method] = error;
       }
     } else {
-      const result = await func().call()
-      info.value[method] = result
+      const result = await func().call();
+      info.value[method] = result;
     }
+  }
+};
+
+async function testName() {
+  // check contract
+  if (!contract.value) {
+    await getContract();
+  }
+
+  // call contract method
+  try {
+    const result = await contract.value.name().call();
+    info.value.name = result;
+  } catch (error) {
+    info.value.name = error;
+  }
+}
+
+async function testSymbol() {
+  // check contract
+  if (!contract.value) {
+    await getContract();
+  }
+
+  // call contract method
+  try {
+    const result = await contract.value.symbol().call();
+    info.value.symbol = result;
+  } catch (error) {
+    info.value.symbol = error;
+  }
+}
+
+async function testDecimals() {
+  // check contract
+  if (!contract.value) {
+    await getContract();
+  }
+
+  // call contract method
+  try {
+    const result = await contract.value.decimals().call();
+    info.value.decimals = result;
+  } catch (error) {
+    info.value.decimals = error;
+  }
+}
+
+async function testBalanceOf() {
+  // check contract
+  if (!contract.value) {
+    await getContract();
+  }
+
+  // call contract method
+  try {
+    const result = await contract.value.balanceOf(defaultAddress).call();
+    info.value.balance = result;
+  } catch (error) {
+    info.value.balance = error;
+  }
+}
+
+async function testTransfer() {
+  // check contract
+  if (!contract.value) {
+    await getContract();
+  }
+
+  // call contract method
+  try {
+    const result = await contract.value
+      .transfer(transferAddress, 1000)
+      .send({ feeLimit: 5e9 });
+    info.value.transfer = result;
+  } catch (error) {
+    info.value.transfer = error;
   }
 }
 </script>
@@ -50,11 +129,11 @@ const getContractMethod = async (method: any) => {
   <main>
     <h1>Tron Test</h1>
     <pre>tronWeb.ready: {{ tronWeb.ready }}</pre>
-    <button @click="getContractMethod('name')">Get Name</button>
-    <button @click="getContractMethod('symbol')">Get Symbol</button>
-    <button @click="getContractMethod('decimals')">Get Decimals</button>
-    <button @click="getContractMethod('balanceOf')">Get Balance</button>
-    <button @click="getContractMethod('transfer')">Transfer</button>
+    <button @click="testName">Get Name</button>
+    <button @click="testSymbol">Get Symbol</button>
+    <button @click="testDecimals">Get Decimals</button>
+    <button @click="testBalanceOf">Get Balance</button>
+    <button @click="testTransfer">Transfer</button>
     <pre>info: {{ info }}</pre>
   </main>
 </template>
